6d362a1f5fd1f9d2beb47b47b249a0aa805dd8af
saitek <saitek300@gmail.com>
2022-12-08 12:42:27 -0800

    working on what doing function, add console table

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+require('console.table');
 
 const db = mysql.createConnection(
     {
@@ -19,7 +19,53 @@ function whatDoing() {
         name: 'start',
         choices: ['view-all-departments', 'view-all-roles', 'view-all-employees', 'add-a-department', 'add-a-role', 'add-an-employee', 'update-an-employee-role']
 
-    }]).then(() => { console.log('hi') })
+    }]).then((answer) => {
+        switch (answer.start) {
+            case 'view-all-departments':
+                db.query('SELECT * FROM department', function (err, results) {
+                   if (err) throw err
+                    console.table(results); whatDoing()
+                }); 
+            break;
+
+            case 'view-all-roles':
+                db.query('SELECT * FROM emp_role', function (err, results) {
+                    if (err) throw err
+                     console.table(results); whatDoing()
+                 });
+
+            break;
+
+            case 'view-all-employees':
+                db.query('SELECT * FROM employee', function (err, results) {
+                    if (err) throw err
+                     console.table(results); whatDoing()
+                 });
+
+            break;
+
+            case 'add-a-department':
+
+            break;
+
+            case 'add-a-role':
+
+            break;
+
+            case 'add-an-employee':
+
+            break;
+
+            case 'update-an-employee-role':
+
+            break;
+
+            default: 
+            return process.exit()
+
+
+        }
+    })
 }
 
 whatDoing()
