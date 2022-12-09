e3643240c8f504eac876189831449857a4ad9112
saitek <saitek300@gmail.com>
2022-12-08 22:50:39 -0800

    finished inquirer function

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 require('console.table');
 
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: process.env.DB_PASSWORD,
         database: 'employees_db'
     },
 );
@@ -23,45 +24,150 @@ function whatDoing() {
         switch (answer.start) {
             case 'view-all-departments':
                 db.query('SELECT * FROM department', function (err, results) {
-                   if (err) throw err
+                    if (err) throw err
                     console.table(results); whatDoing()
-                }); 
-            break;
+                });
+                break;
 
             case 'view-all-roles':
                 db.query('SELECT * FROM emp_role', function (err, results) {
                     if (err) throw err
-                     console.table(results); whatDoing()
-                 });
+                    console.table(results); whatDoing()
+                });
 
-            break;
+                break;
 
             case 'view-all-employees':
                 db.query('SELECT * FROM employee', function (err, results) {
                     if (err) throw err
-                     console.table(results); whatDoing()
-                 });
+                    console.table(results); whatDoing()
+                });
 
-            break;
+                break;
 
             case 'add-a-department':
+                inquirer.prompt([{
+                    type: 'input',
+                    name: "dpt_name",
+                    message: 'what is the name of the department?'
 
-            break;
+                }]).then((answer) => {
+
+                    db.query('INSERT INTO department SET ?', answer, function (err, results) {
+                        if (err) throw err
+                        console.table(results); whatDoing()
+                    })
+                });
+
+                break;
 
             case 'add-a-role':
+                inquirer.prompt([{
+                    type: 'input',
+                    name: "title",
+                    message: 'what is the name of the role?'
+                },
+                {
+                    type: 'number',
+                    name: "salary",
+                    message: 'what is the salary for the role?'
+                },
+                {
+                    type: 'list',
+                    name: "department_id",
+                    message: 'which department does the role belong to?',
+                    choices: ['Sales', 'Engineering', 'Finance', 'Legal']
 
-            break;
+                }]).then(async (answer) => {
+                    const [rows] = await db.promise().query(`SELECT id FROM department WHERE dpt_name = '${answer.department_id}'`)
+                    return { ...answer, id: rows[0].id }
+                }).then((answer) => {
+                    const {
+                        title,
+                        salary,
+                        id
+                    } = answer
+
+                    db.query('INSERT INTO emp_role (title, salary, department_id) VALUES (?,?,?)', [title, salary, id], function (err, results) {
+                        if (err) throw err
+                        console.table(results); whatDoing()
+                    })
+                });
+
+                break;
 
             case 'add-an-employee':
+                inquirer.prompt([{
+                    type: 'input',
+                    name: "first_name",
+                    message: 'what is the first name of the employee?'
+                },
+                {
+                    type: 'input',
+                    name: "last_name",
+                    message: 'what is the last name of the employee?'
 
-            break;
+                },
+                {
+                    type: 'input',
+                    name: "role_id",
+                    message: 'what is the role of the employee?'
+
+                },
+                {
+                    type: 'input',
+                    name: "manager_id",
+                    message: 'who is managing the employee?'
+
+                }]).then(async (answer) => {
+                    const [rows] = await db.promise().query(`SELECT id FROM emp_role WHERE title = '${answer.role_id}'`)
+                    //console.log(rows);
+                    return { ...answer, id: rows[0].id }
+                }).then((answer) => {
+                    const {
+                        first_name,
+                        last_name,
+                        id,
+                        manager_id
+                    } = answer
+                    //console.log(role_id)
+
+                    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?)', [first_name, last_name, id, manager_id], function (err, results) {
+                        if (err) throw err
+                        console.table(results); whatDoing()
+                    })
+                });
+
+                break;
 
             case 'update-an-employee-role':
+                inquirer.prompt([{
+                    type: 'number',
+                    name: "id",
+                    message: "what is the employee id?"
+                },
+                {
+                    type: 'number',
+                    name: 'role_id',
+                    message: "what is their new role id?"
 
-            break;
+                }]).then((answer) => {
+                    const {
+                        id,
+                        role_id
+                    } = answer
 
-            default: 
-            return process.exit()
+                    db.query("UPDATE employee SET role_id = ? WHERE id =?", [role_id, id], function (err, results) {
+                        console.log(answer)
+                        if (err) throw err
+                        console.table(results); whatDoing()
+                    })
+                });
+
+                break;
+
+            default:
+                return process.exit()
 
 
         }
